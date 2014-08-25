cd3f7d336bc5fa4bd5ff814226bf954046a8015f
Iván Mervich <imervich@gmail.com>
2014-08-25 11:05:27 -0500

    Added empty project files

---
+++ AdoptAFriend/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  AdoptAFriend
+//
+//  Created by Iván Mervich on 8/25/14.
+//  Copyright (c) 2014 Iván Mervich - Efrén Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ AdoptAFriend/MainViewController.m
@@ -0,0 +1,23 @@
+//
+//  MainViewController.m
+//  AdoptAFriend
+//
+//  Created by Iván Mervich on 8/25/14.
+//  Copyright (c) 2014 Iván Mervich - Efrén Reyes. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+@end
