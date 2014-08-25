44e374e16fb5e630117bb440180feaffba67f56f
Iván Mervich <imervich@gmail.com>
2014-08-25 16:05:54 -0500

    Added TabBar VC

---
+++ AdoptAFriend/TabBarController.m
@@ -0,0 +1,35 @@
+//
+//  TabBarController.m
+//  AdoptAFriend
+//
+//  Created by Iván Mervich on 8/25/14.
+//  Copyright (c) 2014 Iván Mervich - Efrén Reyes. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+// Segues
+
+// Show login optins screen
+#define showLoginOptionScreenSegue @"showLoginOptionScreenSegue"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+
+	// if not logged in, perform segue
+//	[self performSegueWithIdentifier:showLoginOptionScreenSegue sender:self];
+}
+
+@end
