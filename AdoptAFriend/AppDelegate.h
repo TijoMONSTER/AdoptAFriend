8a7625a7972be806128a9a1d79fbc195cf282e91
Iván Mervich <imervich@gmail.com>
2014-08-25 11:00:43 -0500

    Initial Commit

---
+++ AdoptAFriend/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AdoptAFriend
+//
+//  Created by Iván Mervich on 8/25/14.
+//  Copyright (c) 2014 Iván Mervich - Efrén Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AdoptAFriend/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AdoptAFriend
+//
+//  Created by Iván Mervich on 8/25/14.
+//  Copyright (c) 2014 Iván Mervich - Efrén Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
