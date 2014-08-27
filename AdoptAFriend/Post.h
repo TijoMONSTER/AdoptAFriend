439bdd4ff027315954d97e7a1bbbe71eccec4c0a
Iván Mervich <imervich@gmail.com>
2014-08-27 16:51:28 -0500

    Added Post class (PFObject subclass)

---
+++ AdoptAFriend/Post.h
@@ -0,0 +1,22 @@
+//
+//  Post.h
+//  AdoptAFriend
+//
+//  Created by Iván Mervich on 8/27/14.
+//  Copyright (c) 2014 Iván Mervich - Efrén Reyes. All rights reserved.
+//
+
+@interface Post : PFObject
+
+@property (retain) PFFile *image1;
+@property (retain) PFFile *image2;
+@property (retain) PFFile *image3;
+@property (retain) PFGeoPoint *location;
+@property (retain) NSString *description;
+@property (retain) User *user;
+@property (retain) User *adopter;
+@property BOOL resolved;
+
++ (NSString *)parseClassName;
+
+@end
